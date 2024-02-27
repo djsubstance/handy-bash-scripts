9b7cff2b1f416b36f756e3c58f628d62f5d76955
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-02-27 12:49:35 -0500

    Create OSX_Kill-Burp.sh

---
+++ OSX_Kill-Burp.sh
@@ -0,0 +1,7 @@
+# Many times at least on my MacBook (OSX) 2019 / Intel 8g ram
+# Burp wont die
+
+cat sh/killBurp.sh 
+sudo killall "JavaApplicationStub" "Chromium Helper" "Chromium"
+
+# You may have to run 3 times

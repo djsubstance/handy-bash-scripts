8f7115de6325071ddf675c97f872cb1bf02eaea3
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-03-05 04:01:25 -0500

    Delete JavascriptMustHaves.js

--- JavascriptMustHaves.js
+++
@@ -1,8 +0,0 @@
-// Will check if cookies are enabled if not enable them
-   if (!navigator.cookieEnabled) {
-                window.addEventListener('DOMContentLoaded', function() {
-                    var cookieEl = document.getElementById('cookie-alert');
-                    cookieEl.style.display = 'block';
-                })
-            }
-

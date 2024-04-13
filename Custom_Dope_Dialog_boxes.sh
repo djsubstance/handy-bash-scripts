c964245eac445b01a259de788938afa9dee3ebf0
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-04-13 15:55:47 -0400

    Create Custom_Dope_Dialog_boxes.sh

---
+++ Custom_Dope_Dialog_boxes.sh
@@ -0,0 +1,37 @@
+apt install whiptail -y
+snap install newt 
+
+
+export NEWT_COLORS='
+    root=,black           # Default text on the black background
+    border=white,black    # Border color
+    window=,black         # Window background
+    title=yellow,black    # Title text
+    button=white,black    # Button text
+    actbutton=yellow,black  # Active button text
+    checkbox=white,black  # Checkbox text
+    actcheckbox=yellow,black # Active checkbox text
+    entry=white,black     # Entry field text
+    label=white,black     # Label text
+    listbox=white,black   # Listbox text
+    actlistbox=yellow,black # Active listbox text
+    textbox=white,black   # Textbox text
+    acttextbox=yellow,black # Active textbox text
+    compactbutton=white,black # Compact button text
+    actcompactbutton=yellow,black # Active compact button text
+    sells=white,blue      # Selections
+    sellistbox=white,blue # Selected listbox
+    sellabel=white,blue   # Selected label
+    selentry=white,blue   # Selected entry
+    password=white,black  # Password text
+    disentry=grey,black   # Disabled entry text
+    compactbutton=white,black # Compact button
+    actcompactbutton=yellow,black # Active compact button
+    errmsg=red,black      # Error message text
+    textboxreflowed=white,black # Reflowed text in textboxes
+    emptyscale=,black     # Empty scale
+    fullscale=white,black # Full scale
+    disscale=grey,black   # Disabled scale
+    background=black      # General background
+'
+

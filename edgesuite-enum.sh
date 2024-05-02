2685d51d763a5bd84a2646238797a88e56cd1059
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-05-01 20:01:01 -0400

    Create edgesuite-enum.sh

---
+++ edgesuite-enum.sh
@@ -0,0 +1,1 @@
+curl 'http://edc.edgesuite.net/'   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'   -H 'Accept-Language: en-US,en;q=0.9'   -H 'Cache-Control: no-cache'   -H 'Connection: keep-alive'   -H 'DNT: 1'   -H 'Pragma: no-cache'   -H 'Sec-GPC: 1'   -H 'Upgrade-Insecure-Requests: 1'   -A "kona" --insecure  
